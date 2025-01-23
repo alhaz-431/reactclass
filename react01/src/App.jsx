ed20d082e1537ef1e93d9b638ece661444a2ab6e
alhaz-431 <mohammadalhaz431@gmail.com>
2025-01-23 17:45:56 +0600

    Add files via upload

---
+++ react01/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Navber from './components/Naber/Navber';
+import HeroSection from './components/HeroSection/HeroSection';
+import CardSection from './components/CardSection/CardSection';
+
+const App = () => {
+  return (
+    <div>
+<Navber/>
+<HeroSection/>
+<CardSection/>
+
+    </div>
+  )
+}
+
+export default App;
